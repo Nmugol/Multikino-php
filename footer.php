4ef4d2374a94c1d13a97eaebc5e933152c116cbb
HankaW <wojcicka.hania@gmail.com>
2025-05-05 10:12:41 +0200

    usuniecie
    usuwam stare wersje aby podmienic na nowe aktualne

--- footer.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);  /* Ta linia musi być pierwsza */ ?> 
-<footer class="page-footer mt-auto bg-light border-primary w-100">    
-    <div class="container-fluid"> 
-        <p>© 2025 Multikino</p>
-    </div> 
-</footer>
-<script>
-    document.body.classList.add('d-flex', 'flex-column', 'min-vh-100');
-</script>
